c45374a6eb5a75d7acb98d17f2e05620de68691d
Manav <manav@manavs-mac.local>
2023-06-23 21:49:15 +0530

    Second Commit SvelteLoginForm

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,41 @@
 <script>
-	export let name;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	import { onMount, setContext } from "svelte";
+  
+	import {
+	  key as userContextKey,
+	  initialValue as userContextInitialValue
+	} from "./userContext";
+  
+	import LoginForm from "./LoginForm.svelte";
+  
+	onMount(() => {
+	  setContext(userContextKey, userContextInitialValue);
+	});
+  
+	const submit = ({ email, password }) =>
+	  new Promise((resolve, reject) => {
+		setTimeout(() => {
+		  setContext(userContextKey, {
+			name: "Foo",
+			lastName: "Bar",
+			email: "foo@bar.com"
+		  });
+		  resolve();
+		}, 1000);
+	  });
+  </script>
+  
+  <style>
+	section {
+	  height: 100vh;
+	  width: 100%;
+	  display: flex;
+	  justify-content: center;
+	  align-items: center;
+	  background: linear-gradient(to right, #cd76e2, #e358ab);
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+  </style>
+  
+  <section>
+	<LoginForm {submit} />
+  </section>
